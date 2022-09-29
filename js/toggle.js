09f5f06169149eac65bbeec857fc221e1f13586f
pyojaeyeong <pyojaeyeong@gmail.com>
2022-09-29 10:22:11 +0900

    tutorify

--- js/toggle.js
+++ js/toggle.js
@@ -1,5 +1,5 @@
 const toggle = document.querySelector(".toggle");
-const menu = document.querySelector(".main-wrap");
+const menu = document.querySelector(".depth-wrap");
 
 toggle.addEventListener("click", () => {
   menu.classList.toggle("active");
