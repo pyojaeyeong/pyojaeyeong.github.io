cc6ae41458be6a4d5928a19c93fdd8ca3e6fb3d7
pyojaeyeong <pyojaeyeong@gmail.com>
2022-09-29 12:22:14 +0900

    mobile

--- js/toggle.js
+++ js/toggle.js
@@ -4,3 +4,18 @@ const menu = document.querySelector(".depth-wrap");
 toggle.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var acc_menu = this.nextElementSibling;
+    if (acc_menu.style.display === "block") {
+      acc_menu.style.display = "none";
+    } else {
+      acc_menu.style.display = "block";
+    }
+  });
+}
